71eb411c8a546f9f1085d164a54ee31e567065ed
PavanCM46 <cmpavan200312@gmail.com>
2025-08-16 20:06:19 +0530

    Added sixth testcase of Unit testing

---
+++ Unit_Testing/Unit_testing_6/arr_swap.c
@@ -0,0 +1,16 @@
+#include "demo.h"
+#include "unity.h"
+
+void arr_swap(int arr[], int size) {
+    int *st = arr;
+    int *ed = arr + size - 1;
+
+    while (st < ed) {
+        int temp = *st;
+        *st = *ed;
+        *ed = temp;
+
+        st++;
+        ed--;
+    }
+}
---
+++ Unit_Testing/Unit_testing_6/demo.h
@@ -0,0 +1,4 @@
+#ifndef __DEMO__H
+#define __DEMO__H
+void arr_swap(int arr[],int size);
+#endif
